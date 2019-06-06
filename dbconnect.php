af92b4abb868509cf9679f2289be17a42f1f18e4
cochumo <40910541+cochumo@users.noreply.github.com>
2019-06-06 21:47:57 +0900

    Update dbconnect.php

--- dbconnect.php
+++ dbconnect.php
@@ -1,6 +1,6 @@
 <?php
     try {
-      $db = new PDO('mysql:dbname=mini_service;host=127.0.0.1;charset=utf8','root', 'mamoru');
+      $db = new PDO('mysql:dbname=mini_service;host=127.0.0.1;charset=utf8','root', '******');
       $db->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     } catch (PDOException $e) {
       echo 'DB接続エラー： ' . $e->getMessage();
